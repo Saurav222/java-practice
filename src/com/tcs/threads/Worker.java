20268a5b9c62a9fec8301dfa970ce4cb391730a2
Saurav222 <tcsSaurav123@gmail.com>
2021-08-11 16:22:24 +0530

    multithreading

--- src/com/tcs/threads/Worker.java
+++ src/com/tcs/threads/Worker.java
@@ -5,6 +5,10 @@ public class Worker implements Runnable{
 	@Override
 	public void run() {
 		System.out.println(Thread.currentThread());	
+		try {
+			Thread.sleep(99);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
