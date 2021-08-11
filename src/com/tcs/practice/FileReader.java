e27d7d966b8315767230b016efeb96063d5780c4
Saurav222 <tcsSaurav123@gmail.com>
2021-08-11 12:18:19 +0530

    static imports

---
+++ src/com/tcs/practice/FileReader.java
@@ -0,0 +1,9 @@
+package com.tcs.practice;
+import java.util.Arrays; //normal import
+//import static java.util.Arrays
+public class FileReader {
+	public static void main(String[] args) {
+		int[] numbers = {4,5,6,7,9};
+		Arrays.sort(numbers);
+	}
+}
