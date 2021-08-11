e50c42c52d4c88582d379c3e7700ac3a557ce058
Saurav222 <tcsSaurav123@gmail.com>
2021-08-11 16:26:05 +0530

    thread priority

--- src/com/tcs/threads/Manager.java
+++ src/com/tcs/threads/Manager.java
@@ -7,6 +7,7 @@ public class Manager {
 		for (int i = 0; i < 10; i++) {
 			Worker worker1 = new Worker();
 			Thread t1 = new Thread(worker1);
+			t1.setPriority(9); //thread priority can be in range 1-10 only
 			t1.start();
 			System.out.println(Thread.currentThread().toString() + i);
 		}
