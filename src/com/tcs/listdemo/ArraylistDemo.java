50e2ab9e04efcba86259c2ca4180b456750e63ae
Saurav222 <tcsSaurav123@gmail.com>
2021-08-12 11:45:19 +0530

    functional programming, lambdas, stream

--- src/com/tcs/listdemo/ArraylistDemo.java
+++ src/com/tcs/listdemo/ArraylistDemo.java
@@ -3,11 +3,30 @@ package com.tcs.listdemo;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArraylistDemo {
 	public static void main(String[] args) {
 		List<Integer> numbers = createList();
 		System.out.println(numbers);
+		//sortList(numbers);
+		filterList(numbers);
+	}
+	/**
+	 * Method to filter the elements
+	 * @param numbers
+	 */
+	private static void filterList(List<Integer> numbers) {
+		numbers.forEach((number)->{
+			System.out.println(number%2 == 0);
+		});
+		List<Integer> filtered = numbers.stream()
+				.filter((number)-> number %2 ==0)
+				.collect(Collectors.toList());
+		System.out.println(filtered);
+	}
+
+	private static void sortList(List<Integer> numbers) {
 		numbers.sort(new Comparator<Integer>() {
 			@Override
 			public int compare(Integer o1, Integer o2) {
