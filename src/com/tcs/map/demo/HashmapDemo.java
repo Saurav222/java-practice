3af52e084cb806c5c19dbce6c18cc1eb520005f6
Saurav222 <tcsSaurav123@gmail.com>
2021-08-12 14:26:45 +0530

    maps - Tree and Hash

--- src/com/tcs/map/demo/HashmapDemo.java
+++ src/com/tcs/map/demo/HashmapDemo.java
@@ -2,20 +2,31 @@ package com.tcs.map.demo;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class HashmapDemo {
 	public static void main(String[] args) {
-		createHashMap();
+//		createHashMap();
+		treeMap();
 	}
-
+	private static void treeMap() {
+		// TODO Auto-generated method stub
+		Map<Integer, Float> mathScores = new TreeMap<Integer,Float>();
+		for (int i = 0; i < 20; i++) {
+			mathScores.put(i,new Float(100 * Math.random()));
+		}
+		System.out.println(mathScores);
+	}
 	private static void createHashMap() {
 		// TODO Auto-generated method stub
-		Map<String, Student> mathScores = new HashMap<>();
+		Map<String, Float> mathScores = new HashMap<>();
 		for (int i = 0; i < 20; i++) {
-			mathScores.put("student" + i, new Student(new Float(100 * Math.random()), new Float(100 * Math.random()),
-					new Float(100 * Math.random())));
+			mathScores.put("student" + i,new Float(100 * Math.random()));
 		}
-
+		boolean exists = mathScores.containsKey("student1");
+		System.out.println(exists);
+//		mathScores.containsValue(value);
+		mathScores.put("student1", 34F);
 		System.out.println(mathScores);
 	}
 }
